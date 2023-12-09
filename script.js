6daa30aac338455af49576d7d6e8ae640f8fa6a5
Jackson Raymond <84482662+jackson-07@users.noreply.github.com>
2023-12-09 17:44:19 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+async function getQuote() {
+    try {
+        const response = await fetch('https://api.quotable.io/random');
+        const data = await response.json();
+        return {
+            content: data.content,
+            author: data.author,
+        };
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+        return {
+            content: 'An error occurred',
+        };
+    }
+}
+
+async function updateQuote() {
+    const quoteContainer = document.getElementById('quoteContainer');
+    const { content, author } = await getQuote();
+    quoteContainer.innerHTML = `<blockquote>"${content}"</blockquote><p>- ${author}</p>`;
+}
+
+const getQuoteBtn = document.getElementById('getQuoteBtn');
+getQuoteBtn.addEventListener('click', updateQuote);
