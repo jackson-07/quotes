e145d73f7ccb40a7bf90bdd83538f7d3929ba5c8
Jackson Raymond <84482662+jackson-07@users.noreply.github.com>
2024-04-08 18:06:33 +1000

    removing ;

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 async function getQuote() {
     try {
-        const response = await fetch('https://api.quotable.io/random');
-        const data = await response.json();
+        const response = await fetch('https://api.quotable.io/random')
+        const data = await response.json()
         return {
             content: data.content,
             author: data.author,
         };
     } catch (error) {
-        console.error('Error fetching quote:', error);
-        let errorMessage = error.message;
+        console.error('Error fetching quote:', error)
+        let errorMessage = error.message
         if (error instanceof TypeError && error.message === "Failed to fetch") {
-            errorMessage = "Error 404";
+            errorMessage = "Error 404"
         }
         alert(errorMessage);
         return {
@@ -21,11 +21,11 @@ async function getQuote() {
 
 async function updateQuote() {
     const quoteContainer = document.getElementById('quoteContainer');
-    const { content, author } = await getQuote();
+    const { content, author } = await getQuote()
     if (content !== 'An error occurred') {
-        quoteContainer.innerHTML = `<blockquote>"${content}"</blockquote><p>- ${author}</p>`;
+        quoteContainer.innerHTML = `<blockquote>"${content}"</blockquote><p>- ${author}</p>`
     }
 }
 
-const getQuoteBtn = document.getElementById('getQuoteBtn');
-getQuoteBtn.addEventListener('click', updateQuote);
+const getQuoteBtn = document.getElementById('getQuoteBtn')
+getQuoteBtn.addEventListener('click', updateQuote)
