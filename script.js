0dc49018290d826dfd2c52eb5f30aea6ecaaaa2c
Jackson Raymond <84482662+jackson-07@users.noreply.github.com>
2024-04-13 11:27:11 +1000

    styling code

--- script.js
+++ script.js
@@ -5,27 +5,28 @@ async function getQuote() {
         return {
             content: data.content,
             author: data.author,
-        };
+        }
     } catch (error) {
         console.error('Error fetching quote:', error)
         let errorMessage = error.message
         if (error instanceof TypeError && error.message === "Failed to fetch") {
             errorMessage = "Error 404"
         }
-        alert(errorMessage);
+        alert(errorMessage)
         return {
             content: 'An error occurred',
-        };
+        }
     }
 }
 
 async function updateQuote() {
-    const quoteContainer = document.getElementById('quoteContainer');
+    const quoteContainer = document.getElementById('quoteContainer')
     const { content, author } = await getQuote()
     if (content !== 'An error occurred') {
         quoteContainer.innerHTML = `<blockquote>"${content}"</blockquote><p>- ${author}</p>`
     }
 }
+
 
 const getQuoteBtn = document.getElementById('getQuoteBtn')
 getQuoteBtn.addEventListener('click', updateQuote)
